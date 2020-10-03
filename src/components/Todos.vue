805d580ec3c0d62c9ebcbbbaef0a5533e89749a1
damr <damr18@gmail.com>
2020-10-03 13:03:29 -0500

    display tasks

---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in tasks">
+            <h3>{{todo.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos",
+    props: ["tasks"]
+}
+</script>
+
+<style scoped>
+
+</style>
