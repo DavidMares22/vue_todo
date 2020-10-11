7888f8ae1b3878fd4939d858c2da597394531a4d
damr <damr18@gmail.com>
2020-10-11 11:39:56 -0500

    update tasks

--- src/components/TodoDetail.vue
+++ src/components/TodoDetail.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="details">
-    <h1>Details</h1>
-    <p>{{todo.title}}</p>
-     <button class="del" @click="deleteTodo(todo.id)">X</button>
+  <div class="container">
+   
+   <div class="container-btn">
+       <button class="del" @click="deleteTodo(todo.id)">X</button> 
+   </div>
+    
+
+  <form @submit.prevent="updateTodo(todo.id)">
+
+    <label for="ftitle">Title</label>
+    <input type="text" id="ftitle" name="title" v-model="todo.title">
+
+     <input type="submit" value="Update">
+
+  </form>
+
+  
+
   </div>
 </template>
 
@@ -20,14 +34,29 @@ export default {
     methods:{
         deleteTodo(id){
       // this.todos = this.todos.filter(todo => todo.id != id);
-        axios.delete(`http://127.0.0.1:8085/api/v1/detail/${id}`);// eslint-disable-next-line 
-           
+      let self = this;
+        axios.delete(`http://127.0.0.1:8085/api/v1/detail/${id}`)// eslint-disable-next-line 
+        .then(function(res){
+            self.$router.push({ name: 'Home' });
+        });
     },        
         fetchTask(id){
              axios.get(`http://127.0.0.1:8085/api/v1/detail/${id}`)// eslint-disable-next-line 
         .then(res => this.todo =  res.data)
         .catch(err => console.log(err));   
-        }
+        },
+        
+        updateTodo(id){
+    let self = this;
+      axios.put(`http://127.0.0.1:8085/api/v1/detail/${id}`, {
+        'title':self.todo.title,
+    
+       
+      }).then(function(){
+            self.$router.push({ name: 'Home' });
+        });
+        
+        }        
     },
 
     created(){
@@ -39,13 +68,44 @@ export default {
 
 <style scoped>
 
+.container-btn{
+    text-align: center;
+}
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+ 
+}
+
 .del{
     background: #ff0000;
-    color: #fff;
+    color: white;
+    padding: 12px 20px;
     border: none;
-    padding: 5px 9px;
-    border-radius: 50%;
+    border-radius: 4px;
     cursor: pointer;
-    float: right;
+   
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+
+input[type=text], select, textarea {
+  width: 100%; /* Full width */
+  padding: 12px; /* Some padding */ 
+  border: 1px solid #ccc; /* Gray border */
+  border-radius: 4px; /* Rounded borders */
+  box-sizing: border-box; /* Make sure that padding and width stays in place */
+  margin-top: 6px; /* Add a top margin */
+  margin-bottom: 16px; /* Bottom margin */
+  
 }
 </style>
